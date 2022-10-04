44237cc511c054983fdd42faefe215ffd8566c68
ADMIN2 <muhammedmarshadn@gmail.com>
2022-10-04 17:07:18 +0530

    Commit message

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+using System.Linq;
+
+
+namespace API.Data
+{
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}

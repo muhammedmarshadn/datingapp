0fa35b3a95620e8bcf36f80cd812b9347908de7a
Muhammed Marshad <muhammedmarshadn@gamil.com>
2022-10-19 23:09:48 +0530

    End of section 13

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -6,6 +6,9 @@ import {TabsModule} from 'ngx-bootstrap/tabs'
 import {NgxGalleryModule} from '@kolkov/ngx-gallery'
 import { FileUploadModule } from 'ng2-file-upload';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+import {PaginationModule} from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 
 
 @NgModule({
@@ -19,7 +22,11 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
     TabsModule.forRoot(),
     NgxGalleryModule,
     FileUploadModule,
-    BsDatepickerModule.forRoot()
+    BsDatepickerModule.forRoot(),
+    PaginationModule.forRoot(),
+    ButtonsModule.forRoot(),
+    
+    
   ],
   exports:[
     BsDropdownModule,
@@ -27,7 +34,12 @@ import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
     TabsModule,
     NgxGalleryModule,
     FileUploadModule,
-    BsDatepickerModule
+    BsDatepickerModule,
+    PaginationModule,
+    ButtonsModule,
+
+    
+    
   ]
 })
 export class SharedModule { }
